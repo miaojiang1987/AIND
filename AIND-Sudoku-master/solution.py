91166c07c58afd885cf5774ff12314c32229e2f1
Miao Jiang <miaojiang1987@gmail.com>
2017-08-26 14:19:34 -0400

    Modification

--- AIND-Sudoku-master/solution.py
+++ AIND-Sudoku-master/solution.py
@@ -1,8 +1,8 @@
 assignments = []
 
-def cross(A, B):
+def cross(a, b):
     "Cross product of elements in A and elements in B."
-    return [s+t for s in A for t in B]
+    return [s+t for s in a for t in b]
 
 rows = 'ABCDEFGHI'
 cols = '123456789'
@@ -15,14 +15,20 @@ square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','45
 d1_units = [[rows[i]+cols[i] for i in range(len(rows))]]
 d2_units = [[rows[i]+cols_rev[i] for i in range(len(rows))]]
 
-do_diagonal = 1 # Set this flag = 0 for non-diagonal sudoku
-if do_diagonal == 1:
-    unitlist = row_units + column_units + square_units + d1_units + d2_units
-else:
-    unitlist = row_units + column_units + square_units
 
+unitlist = row_units + column_units + square_units + d1_units + d2_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+def assign_value(values, box, value):
+    """
+    Please use this function to update your values dictionary!
+    Assigns a value to a given box. If it updates the board record it.
+    """
+    values[box] = value
+    if len(value) == 1:
+        assignments.append(values.copy())
+    return values
 
 def assign_value(values, box, value):
     """
@@ -43,30 +49,24 @@ def naked_twins(values):
     """Eliminate values using the naked twins strategy.
     Args:
         values(dict): a dictionary of the form {'box_name': '123456789', ...}
-
     Returns:
         the values dictionary with the naked twins eliminated from peers.
     """
-
-    # First select boxes with 2 entries
     potential_twins = [box for box in values.keys() if len(values[box]) == 2]
-    # Collect boxes that have the same elements
+
     naked_twins = [[box1,box2] for box1 in potential_twins \
                     for box2 in peers[box1] \
                     if set(values[box1])==set(values[box2]) ]
-
-    # For each pair of naked twins,
     for i in range(len(naked_twins)):
         box1 = naked_twins[i][0]
         box2 = naked_twins[i][1]
-        # 1- compute intersection of peers
         peers1 = set(peers[box1])
         peers2 = set(peers[box2])
         peers_int = peers1 & peers2
-        # 2- Delete the two digits in naked twins from all common peers.
         for peer_val in peers_int:
             if len(values[peer_val])>2:
                 for rm_val in values[box1]:
+                    #values[peer_val] = values[peer_val].replace(rm_val,'')
                     values = assign_value(values, peer_val, values[peer_val].replace(rm_val,''))
     return values
 
@@ -109,11 +109,19 @@ def display(values):
 
 
 def eliminate(values):
+    # Write a function that will take as an input, the sudoku in dictionary form,
+    # run through all the boxes, applying the eliminate technique,
+    # and return the resulting sudoku in dictionary form.
+    #print(values)
+
     solved_values = [box for box in values.keys() if len(values[box]) == 1]
-    for box in solved_values:
-        digit = values[box]
-        for peer in peers[box]:
-            values[peer] = values[peer].replace(digit,'')
+
+    for solved_val in solved_values:
+        digit = values[solved_val]
+        peers_solv = peers[solved_val]
+        for peer in peers_solv:
+            #values[peer] = values[peer].replace(digit,'')
+            values = assign_value(values, peer, values[peer].replace(digit,''))
     return values
 
 
@@ -153,15 +161,16 @@ def reduce_puzzle(values):
     return values
 
 def search(values):
+    "Using depth-first search and propagation, create a search tree and solve the sudoku."
     # First, reduce the puzzle using the previous function
     values = reduce_puzzle(values)
-    if values is False:
-        return False ## Failed earlier
-    if all(len(values[s]) == 1 for s in boxes): 
+    if values == False:
+        return False
+
+    if all(len(values[s]) == 1 for s in boxes):
         return values ## Solved!
-    # Choose one of the unfilled squares with the fewest possibilities
+
     n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
-    # Now use recurrence to solve each one of the resulting sudokus, and 
     for value in values[s]:
         new_sudoku = values.copy()
         new_sudoku[s] = value
